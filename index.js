0026442fbe0493c3b1d01b4dbd3cd4f81e42daef
James Batsell <jbfuturedev@gmail.com>
2020-12-13 17:24:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ let main = document.querySelector('main');
 main.remove();
 let newHeader = document.querySelector('h1');
 newHeader.id = "victory";
+newHeader.innerhtml = 'YOUR-NAME is the champion';
